ff284cd5fba77ae34eda7d2205a93c963d355a32
Royex <royexl@proton.me>
2025-06-20 16:56:30 +0200

    Fix bug, update comments
    Fixed bug where last word in given string isn't printed, commented the
    code better

--- nc/center.cpp
+++ nc/center.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 
 vector<string> wrapstring(string str, int max) {
+  // initilize variables for function
   vector<string> words;
   vector<string> wrapped;
   string temp="";
   int linelen=0;
   int wraps=0;
 
+  // loop through string and split it to words
   for(int i=0; i<str.length(); i++) {
     if(str[i]!=' ')
       temp+=str[i];
@@ -18,8 +20,13 @@ vector<string> wrapstring(string str, int max) {
       temp="";
     }
   }
-  temp="";
-
+  // check if temp is empty, if not push it to the back of $words and empty temp  
+  if(temp!="") {
+    words.push_back(temp);
+    temp="";
+  }
+  
+  // loop through $words and when $wrapped child string gets over $max (line length) put it in a new child string
   for(int i=0; i<words.size(); i++) {
     if(linelen+words[i].length()>max) {
       wraps++; 
@@ -34,19 +41,20 @@ vector<string> wrapstring(string str, int max) {
     temp+=' ';
     linelen++;
   }
+  // push last string to the back of $wrapped
   wrapped.push_back(temp);
   return wrapped;
 }
 
 
 int main(int argc, char *argv[]) {
-  // Check for argument, if none return Error
+  // check for arguments, if no string, or no argument for flag return Error
   if(argc<2 || argc%2==1) {
     printf("Error, no argument given");
     return 1;
   }
-  
-  // Create variables
+    
+  // create variables
   int x, y;              // terminal size
   int targetx, targety;  // text center target
   int max=80;            // max line length
@@ -55,10 +63,11 @@ int main(int argc, char *argv[]) {
 
   string arg0 = string(argv[1]);
 
-  // Initilize screen and set invisible cursor
+  // initilize screen and set invisible cursor
   initscr();
   curs_set(0);
 
+  // if there are more than 2 arguments(binaryand string) check for flags and arguments
   if(argc>2) {
     string arg1 = string(argv[2]);
     if(arg1=="-l") {
@@ -66,23 +75,26 @@ int main(int argc, char *argv[]) {
     }
   }
  
-  // Get terminal dimensions and find the center
+  // get terminal dimensions
   getmaxyx(stdscr, y, x);
   if(x<100 || x<max) max=x-20;
-
+  
+  // run &wrapstring on $arg0 (inputted string) and $max (max line length) then return it to $lines vector
   lines = wrapstring(arg0, max);
 
+  // find center of screen horizontally and vertically, also move up vertically by half of the number of lines
   targety = y/2-lines.size()/2;
   targetx = (x-max)/2;
 
-  // Print lines
+  // print each line in $lines
   for(int i=0; i<lines.size(); i++) {
     padding = (max-lines[i].length())/2;
     mvprintw(targety+i, targetx+padding, "%s", lines[i].c_str());
   }
      
-  // Wait for any input and end window
+  // wait for any input and end window
   getch();
   endwin();
+
   return 0;
 }
